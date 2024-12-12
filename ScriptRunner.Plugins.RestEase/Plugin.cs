049b78929c67f4b57011d7815c47cedb984a602b
Peter van de Pas <peter.vdpas@protonmail.com>
2024-12-12 01:52:15 +0100

    pluginSystemVersion: "1.0.26"

--- ScriptRunner.Plugins.RestEase/Plugin.cs
+++ ScriptRunner.Plugins.RestEase/Plugin.cs
@@ -18,7 +18,7 @@ namespace ScriptRunner.Plugins.RestEase;
     description: "A plugin that provides a RestEase RESTful client for ScriptRunner.",
     author: "Peter van de Pas",
     version: "1.0.2",
-    pluginSystemVersion: "1.0.22",
+    pluginSystemVersion: "1.0.26",
     frameworkVersion: ".NET 8.0",
     services: ["IRestEase"])]
 public class Plugin : BaseAsyncServicePlugin
